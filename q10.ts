dfa2f17f3ab0a3ab13fb2791bfd7c1bc093d7408
MuhibALiSiddiqui <162018565+muhibali123@users.noreply.github.com>
2024-04-27 09:22:20 +0500

    Add files via upload

---
+++ q10.ts
@@ -0,0 +1,10 @@
+//                                                                    QUESTION
+
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+//                                                                    ANSWER
+
+let firstname: string = "Muhib Ali";
+let lastname: string = "Siddiqui";
+// console.log(firstname)by this methood it can online print first name
+console.log(`My Name Is ${firstname} ${lastname}`);
---
+++ q9.js
@@ -0,0 +1,6 @@
+//                                                                    QUESTION
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+//                                                                    ANSWER
+let favorite_number = 10;
+console.log(`My Favourite Number is ${favorite_number}`);
+export {};
