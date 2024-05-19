b10a97a82b7d4e0796bc488b20d124426ae319c0
MuhibALiSiddiqui <162018565+muhibali123@users.noreply.github.com>
2024-05-19 21:16:07 +0500

    Delete q5.ts

--- q5.ts
+++
@@ -1,9 +0,0 @@
-//                                                                    QUESTION5
-
-// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message
-
-//                                                                    ANSWER
-
-let famous_person: string = "Saad Hashmani";
-let famous_quote: string = `“Fix Your Relation With Allah He Will Fix Everything.”`;
-console.log(`${famous_person} once said , ${famous_quote}`);
