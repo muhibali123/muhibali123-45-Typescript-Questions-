9165c8247254fbc54185b3b7276f4c8513194611
MuhibALiSiddiqui <162018565+muhibali123@users.noreply.github.com>
2024-05-19 21:17:27 +0500

    Delete q9.js

--- q9.js
+++
@@ -1,6 +0,0 @@
-//                                                                    QUESTION
-// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-//                                                                    ANSWER
-let favorite_number = 10;
-console.log(`My Favourite Number is ${favorite_number}`);
-export {};
