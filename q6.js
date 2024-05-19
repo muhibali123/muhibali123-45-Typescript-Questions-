da5db1144e77c69d156f39c6a01e4181654081f4
MuhibALiSiddiqui <162018565+muhibali123@users.noreply.github.com>
2024-05-19 21:16:23 +0500

    Delete q6.js

--- q6.js
+++
@@ -1,9 +0,0 @@
-//                                                                    QUESTION
-// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
-//                                                                    ANSWER
-let first_name = "Muhib";
-// whitespaces names
-console.log(`Name with White Spaces:\t\n${first_name}\t\n`);
-// stripped names
-console.log("Stripped name : ", first_name.trim());
-export {};
